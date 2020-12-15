c5f59141eb8ff7497c3af7562153b9a2f3371d32
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-15 19:06:49 +0100

    wip

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -1,5 +1,9 @@
 module.exports = (req, res, next) => {
   // pull the token from header
   // verify it
-  
+  const token = req.headers.authorization
+
+  if (!token) {
+    res.status(401).json('we wants token')
+  }
 };
