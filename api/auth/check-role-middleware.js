aa81fd44b6c3e0d4e2da106fff1f80b46c828a54
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-15 19:38:28 +0100

    wip

--- api/auth/check-role-middleware.js
+++ api/auth/check-role-middleware.js
@@ -1,5 +1,5 @@
 module.exports = (role) => (req, res, next) => {
-  if (req.decodedToken.role === 'admin') {
+  if (req.decodedToken.role === role) {
     next()
   } else {
     res.status(403).json('you have no power here')
